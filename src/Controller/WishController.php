91c51f34d971e3918bb9a17f29d8ede2b26a9dd5
Mathilda ALMEIDA <mathilda.almeida2020@campus-eni.fr>
2021-08-25 12:07:05 +0200

    first commit

---
+++ src/Controller/WishController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\WishRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WishController extends AbstractController
+{
+    /**
+     * @Route("/wish", name="wish")
+     */
+    public function wish(WishRepository $repoWish): Response
+    {
+        $tab = $repoWish->findBy(array(), array('dateCreated' => 'DESC'));
+
+        return $this->render('wish/wish.html.twig', [
+            'liste_wish' => $tab,
+        ]);
+    }
+
+    /**
+     * @Route("/wish/detail/{$id}", name="detail")
+     */
+    public function detail(WishRepository $repoWish,$id): Response
+    {
+        $tab = $repoWish->findOneBy($id);
+
+        return $this->render('wish/detail.html.twig', [
+            'id' => $tab->getId() ,
+        ]);
+    }
+
+}
